6dd867aa41e233e5ddeebad02bf6c8e15dcdd33d
repirisyan <repirisyan@icloud.com>
2022-12-22 19:37:31 +0700

    fix detail tagihan

--- resources/views/admin/detail_print_tagihan.blade.php
+++ resources/views/admin/detail_print_tagihan.blade.php
@@ -30,9 +30,12 @@
             </tr>
         </thead>
         <tbody>
+            @php
+                $no = 1;
+            @endphp
             @forelse ($data as $item)
                 @php
-                    $no = 1;
+                    
                     $pemakaian = $item->meteran_akhir - $item->meteran_awal;
                 @endphp
                 <tr>
