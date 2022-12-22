4c766017f03b2c7dcdf1d29da5341a0130f79e14
repirisyan <repirisyan@icloud.com>
2022-12-22 21:08:58 +0700

    fix saldo minus

--- database/migrations/2022_12_20_113038_create_saldos_table.php
+++ database/migrations/2022_12_20_113038_create_saldos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('saldos', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('saldo');
+            $table->unsignedTinyInteger('saldo');
             $table->timestamps();
         });
     }
